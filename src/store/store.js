54fd56ebea4dc162261765637674cf4e3b9f958a
LeNPhu <lenamphu12345@gmail.com>
2023-05-15 21:27:45 +0700

    initial commit

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  ],
+});
+export default store;
