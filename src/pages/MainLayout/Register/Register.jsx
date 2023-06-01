03dfe5e2b7fa045f3c313e47213f72cc27d88b22
Trung Hieu <nthieu1332002@gmail.com>
2023-06-01 23:21:26 +0700

    login thường done(chờ fix api)

--- src/pages/MainLayout/Register/Register.jsx
+++ src/pages/MainLayout/Register/Register.jsx
@@ -7,13 +7,24 @@ import des1 from "../../../assets/images/register-des-1.png";
 import des2 from "../../../assets/images/register-des-2.png";
 import des3 from "../../../assets/images/register-des-3.png";
 import { useRegisterMutation } from "../../../store/services/authApi";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 const Register = () => {
   const navigate = useNavigate();
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [register, { isLoading, isSuccess }] = useRegisterMutation();
+  const [register, { isLoading, isSuccess, error }] = useRegisterMutation();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.data.message);
+    }
+  }, [error]);
+
   const onFinish = (values) => {
     console.log("values", values);
+    Cookies.set("emailTemp", values.email, { expires: 1 / 150, path: "" });
     register(values);
   };
   const onFinishFailed = (errorInfo) => {
@@ -138,7 +149,13 @@ const Register = () => {
           </div>
         </div>
       </div>
-      <Modal open={isSuccess} centered="true" okText="Tiếp tục" onOk={() => navigate("/login")} cancelButtonProps={{ style: { display: 'none' } }}>
+      <Modal
+        open={isSuccess}
+        centered="true"
+        okText="Tiếp tục"
+        onOk={() => navigate("/login")}
+        cancelButtonProps={{ style: { display: "none" } }}
+      >
         <div className="modal-confirmation">
           <img src={logo} alt="" loading="lazy" />
           <h2>XÁC NHẬN TÀI KHOẢN</h2>
--- src/pages/MainLayout/Verify/Verify.jsx
+++ src/pages/MainLayout/Verify/Verify.jsx
@@ -3,15 +3,28 @@ import imiu from "../../../assets/images/imiu-menu.svg";
 import logo from "../../../assets/images/imiu-register.svg";
 import "./styles.scss";
 import { Link, useParams } from "react-router-dom";
-import { Button } from "antd";
-import { useVerifyMutation } from "../../../store/services/authApi";
+import { Button, Space } from "antd";
+import {
+  useEmailMutation,
+  useVerifyMutation,
+} from "../../../store/services/authApi";
 import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import Cookies from "js-cookie";
 
 const Verify = () => {
   const { id, expiration } = useParams();
-  const [verify, { isSuccess, isError }] = useVerifyMutation();
-  // const [searchparams] = useSearchParams();
-  // console.log(searchparams.get("email"));
+  const [
+    verify,
+    { isSuccess: isSuccessVerify, isError: isErrorVerify, error: errorVerify },
+  ] = useVerifyMutation();
+  const [
+    email,
+    { isSuccess: isSuccessEmail, isError: isErrorEmail, error: errorEmail },
+  ] = useEmailMutation();
+
+  const emailTemp =  Cookies.get("emailTemp")
+
   const token = id + "." + expiration + ".";
   console.log("token", token);
 
@@ -21,10 +34,22 @@ const Verify = () => {
     }
   }, [token, verify]);
 
+  useEffect(() => {
+    if (errorVerify) {
+      toast.error(errorVerify.data.message);
+    }
+  }, [errorVerify]);
+
+  useEffect(() => {
+    if (errorEmail) {
+      toast.error(errorEmail.data?.message);
+    }
+  }, [errorEmail]);
+
   return (
     <div className="verify-container">
       <div className="content">
-        {isSuccess && (
+        {isSuccessVerify && (
           <>
             <h1>🎉</h1>
             <h1 className="green">Congratulation!</h1>
@@ -35,15 +60,20 @@ const Verify = () => {
             </Link>
           </>
         )}
-        {isError && (
+        {isErrorVerify && (
           <>
             <h1>😥</h1>
             <h1 className="delete">Confirmation failed!</h1>
             <img src={logo} alt="" />
             <b>Xác thực tài khoản thất bại!</b>
-            <Link to="/register">
-              <Button type="primary">Trở về</Button>
-            </Link>
+            <div className="action">
+              <Button type="primary" onClick={() => email({email: emailTemp})}>Gửi lại mail xác nhận</Button>
+              <Link to="/register">
+                <Button type="default" style={{ marginLeft: "1rem" }}>
+                  Trở về
+                </Button>
+              </Link>
+            </div>
           </>
         )}
       </div>
